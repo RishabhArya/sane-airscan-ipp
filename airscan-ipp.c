b28da890c4d2ebf2991d66d5d22e2981826f81da
rishabharya <rishabharya2799@gmail.com>
2020-10-06 21:29:22 +0530

    removing unwanted comments

--- airscan-ipp.c
+++ airscan-ipp.c
@@ -145,33 +145,22 @@ proto_handler_ipp_new (void)
     ipp->proto.name = "IPP";
     ipp->proto.free = ipp_free;
 
-    /*1*/ipp->proto.devcaps_query = ipp_devcaps_query;
-    /*1*/ipp->proto.devcaps_decode = ipp_devcaps_decode;
+    ipp->proto.devcaps_query = ipp_devcaps_query;
+    ipp->proto.devcaps_decode = ipp_devcaps_decode;
 
-    /*2*/ipp->proto.scan_query = ipp_scan_query;
-    /*2*/ipp->proto.scan_decode = ipp_scan_decode;
+    ipp->proto.scan_query = ipp_scan_query;
+    ipp->proto.scan_decode = ipp_scan_decode;
 
-    /*3*/ipp->proto.load_query = ipp_load_query;
-    /*3*/ipp->proto.load_decode = ipp_load_decode;
+    ipp->proto.load_query = ipp_load_query;
+    ipp->proto.load_decode = ipp_load_decode;
 
-    /*4*/ipp->proto.status_query = ipp_status_query;
-    /*4*/ipp->proto.status_decode = ipp_status_decode;
+    ipp->proto.status_query = ipp_status_query;
+    ipp->proto.status_decode = ipp_status_decode;
 
-    /*5*/ipp->proto.cleanup_query = ipp_cancel_query;
-    /*6*/ipp->proto.cancel_query = ipp_cancel_query;
+    ipp->proto.cleanup_query = ipp_cancel_query;
+    ipp->proto.cancel_query = ipp_cancel_query;
 
     return &ipp->proto;
 }
 
-/*
-JUST FOR UNDERSTANDING TEMP
-1 -> Query/decode scanner capabilities (devcaps_query/devcaps_decode)
-2 -> Initiate scanning (create scan job) and decode result (scan_query/scan_decode)
-3 -> Initiate downloading of scanned image and decode result 
-4 -> Get device status and decode result (status_query/status_decode)
-5 -> Cleanup after scan job (cleanup_query, there is no decode operation)
-6 -> Cancel scan job (cancel_query, and also no decode operation)
-*/
 
-/* vim:ts=8:sw=4:et
- */
